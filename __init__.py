e16ec76dc8042258652c24eaca2cc758e7133a38
James Fraser <fwaggle@fwaggle.org>
2018-06-18 08:34:40 +1000

    Python 3 compatability. Not sure if this broke 2.7, but IDC.

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@ from pelican import signals
 import hashlib
 
 def sha256(input):
-	return hashlib.sha256(input).hexdigest()
+	return hashlib.sha256(input.encode('utf-8')).hexdigest()
 
 def md5(input):
-	return hashlib.md5(input).hexdigest()
+	return hashlib.md5(input.encode('utf-8')).hexdigest()
 
 def add_filter(pelican):
 	pelican.env.filters.update({'sha256': sha256, 'md5': md5})
